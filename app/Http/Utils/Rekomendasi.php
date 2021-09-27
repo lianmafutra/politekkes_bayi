a59b4140c2480ba03212307e6bc5de8c4033861c
lianmafutra <lianmafutra@gmail.com>
2021-09-27 11:02:37 +0700

    fix

--- app/Http/Utils/Rekomendasi.php
+++ app/Http/Utils/Rekomendasi.php
@@ -9,8 +9,8 @@ abstract class Rekomendasi
     const NORMAL = '<p style="text-align: justify;"><strong>Maka perlu dilihat tren pertumbuhannya, Bila tren mengikuti garis pertumbuhan (Naik), maka anak dapat kembali ke Posyandu untuk dipantau pertumbuhannya pada bulan berikutnya.</strong></p> Tekan selanjutnya untuk melakukan penilaian perkembangan balita';
 
     
-    const TIDAK_NORMAL_JAWABAN = '<p style="text-align: justify;"><strong>Maka perlu dikonfirmasi oleh petugas kesehatan yang berkompeten untuk dilakukan: penilaian status gizi berdasarkan indeks BB/U, PB/U atau TB/U, BB/PB dan atau BB/TB, IMT/U</strong></p> Tekan selanjutnya untuk melakukan penilaian perkembangan balita';
-    const NORMAL_JAWABAN = '<p style="text-align: justify;"><strong>Maka perlu dilihat tren pertumbuhannya, Bila tren mengikuti garis pertumbuhan (Naik), maka anak dapat kembali ke Posyandu untuk dipantau pertumbuhannya pada bulan berikutnya.</strong></p> Tekan selanjutnya untuk melakukan penilaian perkembangan balita';
+    const TIDAK_NORMAL_JAWABAN = '<p style="text-align: justify;"><strong>Maka perlu dikonfirmasi oleh petugas kesehatan yang berkompeten untuk dilakukan: penilaian status gizi berdasarkan indeks BB/U, PB/U atau TB/U, BB/PB dan atau BB/TB, IMT/U</strong></p>';
+    const NORMAL_JAWABAN = '<p style="text-align: justify;"><strong>Maka perlu dilihat tren pertumbuhannya, Bila tren mengikuti garis pertumbuhan (Naik), maka anak dapat kembali ke Posyandu untuk dipantau pertumbuhannya pada bulan berikutnya.</strong></p>';
 
 
     public static function getStatusRekomendasi($kode_rekomendasi){
