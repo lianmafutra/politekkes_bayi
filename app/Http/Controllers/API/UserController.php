a8d83f6e81e2dc1f386f3f980f06c47f78339948
lianmafutra <lianmafutra@gmail.com>
2021-09-27 11:56:37 +0700

    fix reset pass

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -66,20 +66,24 @@ class UserController extends Controller
 
     public function resetPassword(Request $request)
     {
-        try {
-            $user =  User::where('nim', $request->nim)->where('username',$request->username)->first();
+        return response()->json([
+            "success" => true,
+            "message" => "sukses"
+        ]);
+        // try {
+        //     $user =  User::where('nim', $request->nim)->where('username',$request->username)->first();
       
-            if($user!=null){
-                $user->password = Hash::make('123456');
-                $user->save();
-                return $this->success("", "Password anda berhasil di reset menjadi 123456");
-            }
-            else{
-                return $this->error("NIM atau username anda tidak tepat ", 200);
-            }
-        } catch (\Throwable $th) {
-            return $this->error("Terjadi Kesalahan".$th, 200);
-        }
+        //     if($user!=null){
+        //         $user->password = Hash::make('123456');
+        //         $user->save();
+        //         return $this->success("", "Password anda berhasil di reset menjadi 123456");
+        //     }
+        //     else{
+        //         return $this->error("NIM atau username anda tidak tepat ", 200);
+        //     }
+        // } catch (\Throwable $th) {
+        //     return $this->error("Terjadi Kesalahan".$th, 200);
+        // }
        
       
     }
